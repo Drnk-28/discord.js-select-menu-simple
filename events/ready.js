8895a9cc3fb947e552515eb7f54a4d644f856c8c
FuadJTM <56724698+FuadJTM@users.noreply.github.com>
2021-10-23 16:45:51 +0700

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,10 @@
+const client = require("../index.js")
+
+client.on("ready", () => {
+  console.log(`${client.user.username} - ready now`)
+  
+})
+
+/*
+ * @INFO https://github.com/FuadJTM/discord.js-select-menu-simple
+ */
